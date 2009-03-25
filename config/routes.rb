694e31e4f4c4bb2e4e1be959ede606c5c499fb43
Jacobo <wolas@jacobo-desktop.(none)>
2009-03-25 15:06:39 +0100

    login box and new task view improvements

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Authlogic routes
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
+  map.root :controller => "user_sessions", :action => 'new'
   map.resource :account, :controller => "users"
   map.resources :users
 
