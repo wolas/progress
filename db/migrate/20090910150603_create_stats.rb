b47db6f28965561a0a7f236221842ce7c425dcf7
Jake <jcpenche@gmail.com>
2009-10-09 17:20:44 +0200

    migrations could not be fucked up anymore

--- db/migrate/20090910150603_create_stats.rb
+++ db/migrate/20090910150603_create_stats.rb
@@ -1,9 +1,11 @@
 class CreateStats < ActiveRecord::Migration
   def self.up
-   
+    create_table :stats, :force => false do |t|
+      t.datetime :time
+    end
   end
 
   def self.down
-    
+    drop_table :stats
   end
 end
