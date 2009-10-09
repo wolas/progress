ced5bb5c71470d2cbf5eb1ce208ed8b03d1a7dbf
Jake <jcpenche@gmail.com>
2009-10-09 17:10:04 +0200

    migrations are fucked up

--- db/migrate/20090804091533_create_users_if_not_present.rb
+++ db/migrate/20090804091533_create_users_if_not_present.rb
@@ -1,28 +1,6 @@
 class CreateUsersIfNotPresent < ActiveRecord::Migration
   def self.up  
-    create_table "users", :force => false do |t|
-      t.string   "surname"
-      t.string   "phone"
-      t.string   "mobile"
-      t.string   "login"
-      t.string   "fax"
-      t.string   "email"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.integer  "company_id"
-      t.boolean  "admin",               :default => false
-      t.string   "skype"
-      t.string   "name"
-      t.string   "access_code"
-      t.string   "avatar_file_name"
-      t.string   "avatar_content_type"
-      t.integer  "avatar_file_size"
-      t.string   "crypted_password",    :default => "",    :null => false
-      t.string   "password_salt",       :default => "",    :null => false
-      t.string   "persistence_token",   :default => "",    :null => false
-      t.string   "website"
-      t.text     "more_information"
-    end
+    
   end
   
   def self.down
