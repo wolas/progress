44398f487e3da91e0824c15e100fe10900d3bd87
Jake <jcpenche@gmail.com>
2009-10-09 17:12:20 +0200

    migrations are fucked up

--- db/migrate/20090910150458_create_companies.rb
+++ db/migrate/20090910150458_create_companies.rb
@@ -1,18 +1,9 @@
 class CreateCompanies < ActiveRecord::Migration
   def self.up
-    create_table "companies", :force => false do |t|
-      t.string "name"
-      t.string "city"
-      t.string "street"
-      t.string "number"
-      t.string "postcode"
-      t.string "phone"
-      t.string "fax"
-      t.string "iva"
-    end
+   
   end
 
   def self.down
-      drop_table :companies
+  
   end
 end
